388bd4a72820ba9c4f750f7194d1ad715ed2cf2a
Maheshwaran <mahemahe17022003@gmail.com>
2024-11-29 17:02:13 +0530

    Updated API URL to point to hosted backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const HTTP_SERVER = express()
+const path = require('path')
 
 HTTP_SERVER.use('/api/cuisines' , require('./Routes/cuisinesRoutes'))
 HTTP_SERVER.use('/api/categories', require('./Routes/categoriesRoutes'))
 HTTP_SERVER.use('/api/subcategories', require('./Routes/subcategoriesRoutes'))
 
-
+HTTP_SERVER.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 module.exports = HTTP_SERVER
--- upload.js
+++ upload.js
@@ -1,17 +1,29 @@
 const multer = require('multer');
 const path = require('path');
 
-// Set up storage engine
+// Define storage location and filename format for uploaded files
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); 
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');  // Store files in 'uploads' directory
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); 
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
   }
 });
 
-
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // Max file size 5 MB
+  fileFilter: function (req, file, cb) {
+    const filetypes = /jpeg|jpg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if (extname && mimetype) {
+      return cb(null, true);
+    } else {
+      cb(new Error('Invalid file type. Only JPEG, JPG, and PNG are allowed.'));
+    }
+  }
+});
 
 module.exports = upload;
